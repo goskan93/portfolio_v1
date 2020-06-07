1e0c96911861703b18c1cc649e5821b06db2360a
goskan93 <natalia.goska@gmail.com>
2020-06-07 14:59:16 -0300

    Updates

---
+++ precache-manifest.d95e959cb6fec76678a31fedb50a44d6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf536421c0f39b4b9ba00a0b8ee190e",
+    "url": "/portfolio_v1/index.html"
+  },
+  {
+    "revision": "3f65539543bc162cd300",
+    "url": "/portfolio_v1/static/css/main.ddee4edd.chunk.css"
+  },
+  {
+    "revision": "9bbf3d9758f27f09caf7",
+    "url": "/portfolio_v1/static/js/2.88d35f12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_v1/static/js/2.88d35f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f65539543bc162cd300",
+    "url": "/portfolio_v1/static/js/main.bc3de487.chunk.js"
+  },
+  {
+    "revision": "85437fa39e61a08d6b4d",
+    "url": "/portfolio_v1/static/js/runtime-main.4cf190ed.js"
+  },
+  {
+    "revision": "6eaa7320cd291cbd08c302547d8b29cc",
+    "url": "/portfolio_v1/static/media/cv-page.6eaa7320.PNG"
+  },
+  {
+    "revision": "832fd0583cb7357d830103b63fe02df9",
+    "url": "/portfolio_v1/static/media/cv.832fd058.pdf"
+  },
+  {
+    "revision": "3acdfef68b2b062e3c70a742c4005244",
+    "url": "/portfolio_v1/static/media/guess-number-app.3acdfef6.gif"
+  },
+  {
+    "revision": "cc8f962658bf99a3aac2cf7c47987532",
+    "url": "/portfolio_v1/static/media/me.cc8f9626.jpg"
+  },
+  {
+    "revision": "391eac5c532b8edb65a95ce163c2860c",
+    "url": "/portfolio_v1/static/media/playground-page.391eac5c.gif"
+  },
+  {
+    "revision": "50220547a1c2399c71eea16e1b48c377",
+    "url": "/portfolio_v1/static/media/recipes-app.50220547.gif"
+  }
+]);
